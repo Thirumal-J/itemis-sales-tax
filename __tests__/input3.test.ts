96a5e63574553dfd42c2f4134e4fa0d82902bfbe
Thirumal-J <thirumal.janakiraman@gmail.com>
2022-12-04 02:04:12 +0100

    feat(test-cases)-done

---
+++ __tests__/input3.test.ts
@@ -0,0 +1,88 @@
+import { describe } from "@jest/globals";
+import supertest from "supertest";
+import app from "../src/app";
+
+describe("POST calculate bill", () => {
+  it("Given input 2", async () => {
+    const mockRequest = [
+      {
+        name: "perfume",
+        category: "others",
+        price: 27.99,
+        quantity: 1,
+        isImported: true,
+      },
+      {
+        name: "perfume",
+        category: "others",
+        price: 18.99,
+        quantity: 1,
+        isImported: false,
+      },
+      {
+        name: "headache pills",
+        category: "medicine",
+        price: 9.75,
+        quantity: 1,
+        isImported: false,
+      },
+      {
+        name: "chocolates",
+        category: "food",
+        price: 11.25,
+        quantity: 1,
+        isImported: true,
+      },
+    ];
+
+    const mockResponse = {
+      totalItems: [
+        {
+          name: "perfume",
+          category: "others",
+          price: 27.99,
+          quantity: 1,
+          isImported: true,
+          priceWithTax: 32.19,
+          itemTax: 4.2,
+        },
+        {
+          name: "perfume",
+          category: "others",
+          price: 18.99,
+          quantity: 1,
+          isImported: false,
+          priceWithTax: 20.89,
+          itemTax: 1.9,
+        },
+        {
+          name: "headache pills",
+          category: "medicine",
+          price: 9.75,
+          quantity: 1,
+          isImported: false,
+          priceWithTax: 9.75,
+          itemTax: 0,
+        },
+        {
+          name: "chocolates",
+          category: "food",
+          price: 11.25,
+          quantity: 1,
+          isImported: true,
+          priceWithTax: 11.85,
+          itemTax: 0.6,
+        },
+      ],
+      totalSalesTax: 6.7,
+      totalCost: 74.68,
+    };
+
+    const response = await supertest(app)
+      .post(`/calculate-bill`)
+      .send(mockRequest);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(mockResponse);
+  });
+});
---
+++ __tests__/validatingCategory.test.ts
@@ -0,0 +1,28 @@
+import { describe } from "@jest/globals";
+import supertest from "supertest";
+import app from "../src/app";
+
+describe("POST calculate bill", () => {
+  it("Given wrong item category should return error message", async () => {
+    const mockRequest = [
+      {
+        name: "perfume",
+        category: "perfume",
+        price: 27.99,
+        quantity: 1,
+        isImported: true,
+      }
+    ];
+
+    const mockResponse = {
+        errorMsg:"One or more item category is not from the allowed categories"
+    };
+
+    const response = await supertest(app)
+      .post(`/calculate-bill`)
+      .send(mockRequest);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toEqual(mockResponse);
+  });
+});
